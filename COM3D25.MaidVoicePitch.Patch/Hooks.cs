eaeeddad43ca169187f7ea6645ee578f79e7170f
lilly <lilly1987@naver.com>
2022-03-17 00:07:14 +0900

    초기 커밋.

---
+++ COM3D25.MaidVoicePitch.Patch/Hooks.cs
@@ -0,0 +1,22 @@
+﻿using CM3D2.ExternalSaveData.Managed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COM3D25.MaidVoicePitch.Patch
+{
+    internal class Hooks
+    {
+        static string[] plugins = new string[] { "CM3D2.MaidVoicePitch" };
+
+
+        internal static void ForeArmFix(Maid maid)
+        {
+            foreach (string pluginName in plugins)
+            {
+                ExSaveData.SetBool(maid, pluginName, "FARMFIX", false);
+            }
+        }
+    }
+}
---
+++ COM3D25.MaidVoicePitch.Patch/Patcher.cs
@@ -0,0 +1,60 @@
+﻿using BepInEx.Logging;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace COM3D25.MaidVoicePitch.Patch
+{
+    public class Patcher
+    {
+        // List of assemblies to patch
+        public static IEnumerable<string> TargetDLLs { get; } = new[] { "COM3D2.MaidVoicePitch.Plugin.dll" };
+
+        public static ManualLogSource log = Logger.CreateLogSource("MaidVoicePitchFix");
+
+        public static void Initialize()
+        {
+            log.LogMessage($"Initialize");
+            log.LogMessage($"Initialize");
+        }
+
+        // Patches the assemblies
+        public static void Patch(AssemblyDefinition assembly)
+        {
+            // Patcher code here
+            log.LogMessage($"Initialize {assembly.Name}");
+
+            TypeDefinition type2 = AssemblyDefinition.ReadAssembly(Assembly.GetExecutingAssembly().Location).MainModule.GetType("COM3D25.MaidVoicePitch.Patch.Hooks");
+            MethodDefinition method2 = type2.Methods.FirstOrDefault((MethodDefinition m) => m.Name.Equals("ForeArmFix"));
+
+            TypeDefinition type = assembly.MainModule.GetType("CM3D2.MaidVoicePitch.Plugin", "ForeArmFixOptimized");
+            MethodDefinition method = type.Methods.FirstOrDefault((MethodDefinition m) => m.Name.Equals("ForeArmFix"));
+
+            ILProcessor ilProcessor = method.Body.GetILProcessor();
+
+            // if ForeArmFix method Exception 
+            // than call ExSaveData.SetBool(maid, MaidVoicePitch.PluginName, "FARMFIX", false);
+
+            ExceptionHandler handler = new ExceptionHandler(ExceptionHandlerType.Catch)
+            {
+                TryStart = method.Body.Instructions.First(),
+                TryEnd = method.Body.Instructions.Last(),
+                HandlerStart = method2.Body.Instructions.First(),
+                HandlerEnd = method2.Body.Instructions.Last(),
+                //CatchType = method.Import(typeof(Exception)),
+            };
+
+            method.Body.ExceptionHandlers.Add(handler);
+        }
+
+        public static void Finish()
+        {
+            log.LogMessage($"Finish");
+        }
+    }
+
+}
